6191e818b6d8ce4eef481ab2114b8855c019d108
Inquis1t0r <pi0tr3k9292@gmail.com>
2020-02-17 22:56:21 +0100

    Create main.java

---
+++ Console/main.java
@@ -0,0 +1,9 @@
+class Main {
+  public static void main(String[] args) {
+        double wzrost = 1.74; //w metrach
+        double waga = 65; //w kg
+        double bmi= (waga / (wzrost*wzrost));
+
+        System.out.println("BMI WYNOSI = " + bmi);
+  }
+}
